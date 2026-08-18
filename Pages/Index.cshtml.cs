f5b1aa3571e5e15dce23540d3ea95b7e7123f697
BN <bcn@live.in>
2026-08-19 00:01:21 +0530

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App_ASP.Net.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
